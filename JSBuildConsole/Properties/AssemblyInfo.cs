c7f269df83b819e18a1fa9211cfb59d9e57f984d
bmoeskau <bmoeskau@9c490523-2b22-0410-b8fe-7ba743be0f5d>
2006-11-28 08:50:45 +0000

    - Removed try/catch from ProjectBuilder.Build so that the console can properly exit on error - Added try/catch around calls to ProjectBuilder.Build in both console and GUI - Added BuildComplete event to ProjectBuilder so that we could properly exit the build process in each calling process - Added /verbose command line argument for the console and logic to honor the verbose flag while building - Added return code -99 on error in console so that calling processes (NAnt) can properly identify the build failure

--- JSBuildConsole/Properties/AssemblyInfo.cs
+++ JSBuildConsole/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("JSConsole")]
+[assembly: AssemblyTitle("JSBuildConsole")]
 [assembly: AssemblyDescription("JS Builder Console App")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jack Slocum")]
